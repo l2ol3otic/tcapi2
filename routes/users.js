687f78816f2d6fb80949a657449d57a545322a70
l2ol3otic <l2ol3otic@gmail.com>
2018-09-04 16:58:36 +0700

    connect sql tedious

--- routes/users.js
+++ routes/users.js
@@ -1,41 +1,76 @@
 var express = require('express');
 var router = express.Router();
 var sql = require('mssql');
+var TYPES = require('tedious').TYPES;
+
+var Connection = require('tedious').Connection;
+var Request = require('tedious').Request;
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 router.get('/tid', function (req, res, next) {
+  ConnData()
+  res.send("Bigman")
+});
+router.get('/tid2', function (req, res, next) {
+  ConnData()
   res.send(ConnData())
+ 
 });
 
 function ConnData() {
+  var x = "test"
 
-  const sqlConfig = {
+  const sqlConfig2 = {
     server: '45.77.38.34',
-    user: 'sa',
+    userName: 'sa',
     password: 'critical4875',
-    driver: 'tedious',
-    database: 'SP4807',
+    //driver: 'tedious',
     //encrypt: false,
     options: {
-      instanceName: 'sql',
-      encrypt: false,
+      encrypt: true,
+      database: 'SP4807',
     }
     //driver: 'MSSQLSERVER',
   }
-
-  sql.connect(sqlConfig, function () {
-    var request = new sql.Request();
-    var sd = "admin"
-    request.query("select * from dbo.userTest Where = '?'", sd, function (err, result, fields) {
-      if (err) console.log(err);
-      console.log(result)
-      res.end(JSON.stringify(recordset)); // Result in JSON format
-      sql.close()
-      console.log("Succecss")
-    });
+  
+  var connection = new Connection(sqlConfig2)
+  connection.on('connect', function(err) {
+    if (err) {
+      console.log("EROR787898",err);
+    } else {
+      executeStatement();
+    }
   });
+  function executeStatement() {
+    var sd = "admin"
+    var sqlcommand = "SELECT * FROM userTest WHERE username = @username"
+    request = new Request(sqlcommand, function(err, rowCount) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(rowCount + ' rows');
+      }
+      connection.close();
+    });
+  
+    request.on('row', function(columns) {
+      columns.forEach(function(column) {
+        if (column.value === null) {
+          console.log('NULL');
+        } else {
+          console.log(column.value);
+        }
+      });
+    });
+
+    request.addParameter('username', TYPES.VarChar, sd);
+  
+    connection.execSql(request);
+  }
+  return x;
+  
 }
 module.exports = router;
